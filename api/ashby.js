836d8cfaf6fe89704b5c7cece7af154decc8d2b3
WebflowPhenomen <webflow@phenomenon-studio.com>
2025-06-25 17:28:52 +0300

    initial commit

--- api/ashby.js
+++ api/ashby.js
@@ -1,14 +1,14 @@
 export default async function handler(req, res) {
-  const response = await fetch("https://api.ashbyhq.com/v1/job-postings", {
-    headers: {
-      "Authorization": `Bearer ${process.env.a45cfef17a96ebd8c0da266910131be656d46798b7dda8e8076d59ef5b3dbdc6}`
-    }
-  });
+	const response = await fetch("https://api.ashbyhq.com/v1/job-postings", {
+		headers: {
+			"Authorization": `Bearer ${process.env.ASHBY_API_KEY}`
+		}
+	});
 
-  if (!response.ok) {
-    return res.status(response.status).json({ error: 'Ashby API error' });
-  }
+	if (!response.ok) {
+		return res.status(response.status).json({ error: 'Ashby API error' });
+	}
 
-  const data = await response.json();
-  res.status(200).json(data);
+	const data = await response.json();
+	res.status(200).json(data);
 }
