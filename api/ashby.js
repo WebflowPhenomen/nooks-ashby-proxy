77ed3b4501a4ce2118a33e250e90274898d43207
WebflowPhenomen <webflow@phenomenon-studio.com>
2025-06-25 18:08:12 +0300

    update ashby.js

--- api/ashby.js
+++ api/ashby.js
@@ -1,5 +1,6 @@
 export default async function handler(req, res) {
-	const response = await fetch("https://api.ashbyhq.com/v1/job-postings", {
+	// const response = await fetch("https://api.ashbyhq.com/v1/job-postings", {
+	const response = await fetch("https://api.ashbyhq.com/posting-api/job-board/nooks?includeCompensation=true", {
 		headers: {
 			"Authorization": `Bearer ${process.env.ASHBY_API_KEY}`
 		}
